6587de06b3891882db98981a35153aac868ff1d3
Gustavo <gutigimenez15@gmail.com>
2025-03-06 15:27:29 -0300

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Board from './Board'
+
+function App() {
+
+  return (
+    <>
+      <div className='game-board'>
+        <Board />
+      </div>
+    </>
+  )
+}
+
+export default App
